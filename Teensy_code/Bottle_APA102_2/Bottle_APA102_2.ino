917e5e876c5da6c55778492b9f86fe3b7fdf86d1
Susan Spangenberg <susanspan12@gmail.com>
2018-07-27 15:00:59 +0200

    Teensy code for 3 LED controllers

---
+++ Teensy_code/Bottle_APA102_2/Bottle_APA102_2.ino
@@ -0,0 +1,419 @@
+#include "FastLED.h"
+
+//----#DEFINES--------------------------------------------------------
+#define NUM_LEDS 145   
+
+//****PIN DEFINES*****************************************************
+#define SPI_DATA_PIN 11
+#define SPI_CLK_PIN 13
+#define DEBUG_BUTTON 14
+
+//*****ANIMATIONS*****************************************************
+#define ANIMATION_TIME 30000
+#define DEFAULT_TIME 60000
+
+#define FORWARD 0
+#define BACKWARD 1
+#define MEDIUM 50
+#define FAST 5
+
+#define BRIGHTNESS          96
+#define FRAMES_PER_SECOND  120
+
+#define DEBUG_MODE false
+
+//----Variable definitions--------------------------------------------
+
+//****SERIAL COMMS*****************************************************
+
+//Run animation 1 to 5 
+const byte ANIMATION_BYTE_1 = '1';
+const byte ANIMATION_BYTE_2 = '2';
+const byte ANIMATION_BYTE_3 = '3';
+const byte ANIMATION_BYTE_4 = '4';
+const byte ANIMATION_BYTE_5 = '5';
+
+//****Colour HSV******************************************************
+byte hue = 0;
+byte saturation = 255;
+byte value = 100;
+byte animation_byte = 'x';
+
+boolean direction = FORWARD;    //for animations
+
+//****LED strip*******************************************************
+const int THEME_HUE_BLUE = 0;
+const int THEME_HUE_PURPLE = 230;
+const int THEME_HUE_PINK = 190;
+const int THEME_HUE_AQUA = 35;
+const int THEME_HUE_YELLOW = 140;
+
+const int chsv_orange = 150;
+
+int theme_hues[] = {THEME_HUE_BLUE, THEME_HUE_PURPLE, THEME_HUE_PINK, THEME_HUE_AQUA, THEME_HUE_YELLOW};
+
+int random_index_led_1 = -1;
+int random_index_led_2 = -1;
+float animation_timer = -1;
+float default_timer = -1;
+
+CRGB leds[NUM_LEDS];
+
+//-------------------------------Function prototypes-----------------------------------------------------------------------------
+void rainbow(int cycles, int speed);
+void stripes(CRGB c1, CRGB c2, int width);
+void colorWipe(CHSV c, int speed_);
+void allColor();
+void someColour(CHSV c, int start_led, int end_led);
+void disolve(CHSV c, int speed_);
+void moving_strip(CHSV c, int speed_, int width_);
+void default_animation_1();
+void default_animation_2();
+void default_animation_3();
+void default_animation_4();
+void default_animation_5();
+void default_animation_6();
+void default_animation_7();
+
+void setup() {
+  Serial.begin(9600);
+  Serial1.begin(9600);
+  delay(2000);  //Long delay for boot stability
+  FastLED.addLeds<APA102, SPI_DATA_PIN, SPI_CLK_PIN, RGB, DATA_RATE_MHZ(10)>(leds, NUM_LEDS);
+}
+
+void loop() {
+  default_animation_1();    //Rainbow scrolling
+  default_animation_2();    //Random lines of colour appearing
+  default_animation_3();    //Colour wipe using theme hues
+  default_animation_4();    //moving strip of light, changing colour according to theme hues
+  default_animation_5();    //Sparkle/flashes
+  default_animation_6();    //Rainbow colours fade in and out
+  default_animation_7();    //Sparkle/flashes
+  default_animation_4();
+  default_animation_2();
+}
+
+//--------------------Serial comms------------------------------------------------------
+void check_serial_incoming() {
+  if (Serial1.available()) {
+    animation_byte = Serial1.read();
+    if (animation_byte == ANIMATION_BYTE_1) {
+      animation_1();
+    }
+    else if (animation_byte == ANIMATION_BYTE_2) {
+      animation_2();
+    }
+    else if (animation_byte == ANIMATION_BYTE_3) {
+      animation_3();
+    }
+    else if (animation_byte == ANIMATION_BYTE_4) {
+      animation_4();
+
+    }
+    else if (animation_byte == ANIMATION_BYTE_5) {
+      animation_5();
+    }
+  }
+  while (Serial1.available()) {  //clear buffer
+    Serial1.read();
+  }
+}
+
+//----------------------Default Animations-------------------------------------
+//----------------------Default 1-------------------------------------
+void default_animation_1() {
+  colorWipe(CHSV (0, 0, 0), 1);
+  default_timer = millis();
+  while (millis() - default_timer < DEFAULT_TIME) {
+    if (DEBUG_MODE) {
+      Serial.println('default 1');
+    }
+    rainbow(10, 8);
+  }
+}
+
+//----------------------Default 2-------------------------------------
+void default_animation_2() {
+  colorWipe(CHSV (0, 0, 0), 1);
+  default_timer = millis();
+  while (millis() - default_timer < DEFAULT_TIME) {
+    if (DEBUG_MODE) {
+      Serial.println('default 7');
+    }
+    random_index_led_1 = random(NUM_LEDS);
+    random_index_led_2 = random(NUM_LEDS);
+    someColour(CHSV (random(0, 255), 255, 255), min(random_index_led_1, random_index_led_2), max(random_index_led_1, random_index_led_2));
+    delay(300);
+  }
+}
+
+//----------------------Default 3-------------------------------------
+void default_animation_3() {
+  colorWipe(CHSV (0, 0, 0), 1);
+  default_timer = millis();
+  while (millis() - default_timer < DEFAULT_TIME) {
+    if (DEBUG_MODE) {
+      Serial.println('default 3');
+    }
+    for (int i = 4; i >= 0; i--) {
+      colorWipe(CHSV (theme_hues[i], 255, 255), 5);
+    }
+  }
+}
+
+//----------------------Default 4-------------------------------------
+void default_animation_4() {
+  colorWipe(CHSV (0, 0, 0), 1);
+  default_timer = millis();
+  while (millis() - default_timer < DEFAULT_TIME) {
+    if (DEBUG_MODE) {
+      Serial.println('default 6');
+    }
+    for (int i = 4; i >= 0; i--) {
+      moving_strip(CHSV (theme_hues[i], 255, 255), 10, 50);
+    }
+  }
+}
+
+//----------------------Default 5-------------------------------------
+void default_animation_5() {
+  colorWipe(CHSV (0, 0, 0), 1);
+  default_timer = millis();
+  while (millis() - default_timer < DEFAULT_TIME) {
+    if (DEBUG_MODE) {
+      Serial.println('default 2');
+    }
+    lightning(CHSV (THEME_HUE_BLUE, 200, 255), 20, 50, MEDIUM / 1.5);
+  }
+}
+
+//----------------------Default 6-------------------------------------
+void default_animation_6() {
+  int i = 0;
+  colorWipe(CHSV (0, 0, 0), 1);
+  allColor(CHSV (THEME_HUE_BLUE, 255, 255));
+  default_timer = millis();
+  while (millis() - default_timer < DEFAULT_TIME) {
+    if (DEBUG_MODE) {
+      Serial.println(i);
+    }
+    i+=20;
+    disolve(CHSV (i, 255, 255), 5);
+    disolve(CHSV (0, 0, 0), 5);
+    if (i >= 255) {
+      i = 0;
+    }
+  }
+}
+
+//----------------------Default 7-------------------------------------
+void default_animation_7() {
+  colorWipe(CHSV (0, 0, 0), 1);
+  default_timer = millis();
+  while (millis() - default_timer < DEFAULT_TIME) {
+    if (DEBUG_MODE) {
+      Serial.println('default 8');
+    }
+    lightning(CHSV(THEME_HUE_PINK, 255, 255), 20, 50, MEDIUM / 1.5);
+    lightning(CHSV(THEME_HUE_PURPLE, 255, 255), 20, 50, MEDIUM / 1.5);
+  }
+}
+
+
+//----------------------1-5 Animations-------------------------------------
+
+void animation_1() {
+  animation_timer = millis();
+  allColor(CHSV (chsv_orange, 255, 255));
+  while (millis() - animation_timer < ANIMATION_TIME) {
+    disolve(CHSV (THEME_HUE_BLUE, 255, 255), 10);
+    disolve(CHSV (chsv_orange, 255, 255), 10);
+  }
+  colorWipe(CHSV (0, 0, 0), 1);
+}
+
+void animation_2() {
+  colorWipe(CHSV (0, 0, 0), 1);
+  animation_timer = millis();
+  while (millis() - animation_timer < ANIMATION_TIME) {
+    lightning(CHSV(THEME_HUE_PURPLE, 255, 255), 20, 50, MEDIUM);
+  }
+  colorWipe(CHSV (0, 0, 0), 1);
+}
+
+void animation_3() {
+  animation_timer = millis();
+  while (millis() - animation_timer < ANIMATION_TIME) {
+    allColor(CHSV (THEME_HUE_BLUE, 255, 255));
+    delay(100);
+    allColor(CHSV (THEME_HUE_PURPLE, 255, 255));
+    delay(100);
+    allColor(CHSV (THEME_HUE_PINK, 255, 255));
+    delay(100);
+    allColor(CHSV (THEME_HUE_AQUA, 255, 255));
+    delay(100);
+    allColor(CHSV (THEME_HUE_YELLOW, 255, 255));
+    delay(100);
+  }
+  colorWipe(CHSV (0, 0, 0), 1);
+}
+
+
+void animation_4() {
+  animation_timer = millis();
+  while (millis() - animation_timer < ANIMATION_TIME) {
+    moving_strip(CHSV (THEME_HUE_AQUA, 255, 255), 2, 50);
+  }
+  colorWipe(CHSV (0, 0, 0), 1);
+}
+
+void animation_5() {
+  animation_timer = millis();
+  allColor(CHSV (THEME_HUE_YELLOW, 255, 255));
+  while (millis() - animation_timer < ANIMATION_TIME) {
+    disolve(CHSV (0, 0, 0), 6);
+    disolve(CHSV (THEME_HUE_YELLOW, 255, 255), 6);
+    colorWipe(CHSV (0, 0, 0), 3);
+    colorWipe(CHSV (THEME_HUE_YELLOW, 255, 255), 3);
+  }
+  colorWipe(CHSV (0, 0, 0), 1);
+}
+
+//----------------------Animation functions-------------------------------------
+
+void lightning(CRGB c, int simultaneous, int cycles, int speed) {
+  int flashes[simultaneous];
+
+  for (int i = 0; i < cycles; i++) {
+    for (int j = 0; j < simultaneous; j++) {
+      int idx = random(NUM_LEDS);
+      flashes[j] = idx;
+      leds[idx] = c;
+    }
+    FastLED.show();
+    check_serial_incoming();
+    delay(speed);
+    for (int s = 0; s < simultaneous; s++) {
+      leds[flashes[s]] = CRGB::Black;
+    }
+    delay(speed);
+  }
+}
+
+void allColor(CHSV c) {
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = c;
+  }
+  FastLED.show();
+  check_serial_incoming();
+}
+
+void someColour(CHSV c, int start_led, int end_led) {
+  for (int i = start_led; i < end_led; i++) {
+    leds[i] = c;
+  }
+  FastLED.show();
+  check_serial_incoming();
+}
+
+// Random disolve colors
+void disolve(CHSV c, int speed_) {
+  int simultaneous = 5;
+  int cycles = 80;
+  for (int i = 0; i < cycles; i++) {
+    for (int j = 0; j < simultaneous; j++) {
+      int idx = random(NUM_LEDS);
+      leds[idx] = c;
+    }
+    FastLED.show();
+    check_serial_incoming();
+    delay(speed_);
+  }
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = c;
+  }
+  FastLED.show();
+  check_serial_incoming();
+}
+
+// Rainbow colors that slowly cycle across LEDs
+void rainbow(int cycles, int speed) { // TODO direction
+  if (cycles == 0) {
+    for (int i = 0; i < NUM_LEDS; i++) {
+      leds[i] = Wheel(((i * 256 / NUM_LEDS)) & 255);
+    }
+    FastLED.show();
+    check_serial_incoming();
+  }
+  else {
+    for (int j = 0; j < 256 * cycles; j++) {
+      for (int i = 0; i < NUM_LEDS; i++) {
+        leds[i] = Wheel(((i * 256 / NUM_LEDS) + j) & 255);
+      }
+      FastLED.show();
+      check_serial_incoming();
+      delay(speed);
+    }
+  }
+}
+
+// Sliding bar across LEDs
+void moving_strip(CHSV c, int speed_, int width_) {
+  // First slide the leds in one direction
+  for (int i = 0; i <= NUM_LEDS - width_; i++) {
+    for (int j = 0; j < width_; j++) {
+      leds[i + j] = c;
+    }
+    FastLED.show();
+    check_serial_incoming();
+    for (int j = 0; j < 5; j++) {
+      leds[i + j] = CRGB::Black;
+    }
+    delay(speed_);
+  }
+  delay(50);
+  for (int i = NUM_LEDS - width_; i >= 0; i--) {
+    for (int j = 0; j < width_; j++) {
+      leds[i + j] = c;
+    }
+    FastLED.show();
+    check_serial_incoming();
+    for (int j = 0; j < width_; j++) {
+      leds[i + j] = CRGB::Black;
+    }
+    delay(speed_);
+  }
+  delay(50);
+}
+
+void colorWipe(CHSV c, int speed_) {
+  bool direction_ = 1;
+  for (int i = 0; i < NUM_LEDS; i++) {
+    if (direction == FORWARD) {
+      leds[i] = c;
+    }
+    else {
+      leds[NUM_LEDS - 1 - i] = c;
+    }
+    FastLED.show();
+    check_serial_incoming();
+    delay(speed_);
+  }
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+CRGB Wheel(byte WheelPos) {
+  if (WheelPos < 85) {
+    return CRGB(WheelPos * 3, 255 - WheelPos * 3, 0);
+  }
+  else if (WheelPos < 170) {
+    WheelPos -= 85;
+    return CRGB(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  else {
+    WheelPos -= 170;
+    return CRGB(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+}
---
+++ Teensy_code/RGB_Frames/RGB_Frames.ino
@@ -0,0 +1,493 @@
+#include "FastLED.h"
+
+//----#DEFINES--------------------------------------------------------
+//****PIN DEFINES*****************************************************
+#define R_1_PWM_PIN 35
+#define G_1_PWM_PIN 36
+#define B_1_PWM_PIN 37
+#define R_2_PWM_PIN 38
+#define G_2_PWM_PIN 14
+#define B_2_PWM_PIN 16
+#define R_3_PWM_PIN 20
+#define G_3_PWM_PIN 21
+#define B_3_PWM_PIN 22
+#define R_4_PWM_PIN 23
+#define G_4_PWM_PIN 2
+#define B_4_PWM_PIN 3
+#define R_5_PWM_PIN 4
+#define G_5_PWM_PIN 5
+#define B_5_PWM_PIN 6
+#define R_6_PWM_PIN 7
+#define G_6_PWM_PIN 8
+#define B_6_PWM_PIN 9
+#define DEBUG_BUTTON_PIN 34
+#define RELAY_PIN 39
+
+#define ANIMATION_TIME 30000    
+#define DEFAULT_ANIMATION_TIME 60000    
+#define RELAY_DELAY_TIME 4000    
+#define RELAY_START_TIME 2000    
+
+#define DEBUG_PRINTLN false
+
+//****SERIAL COMMS*****************************************************
+
+//Run animation 1 to 5 (0x01 to 0x05)
+const byte ANIMATION_BYTE_1 = '1';
+const byte ANIMATION_BYTE_2 = '2';
+const byte ANIMATION_BYTE_3 = '3';
+const byte ANIMATION_BYTE_4 = '4';
+const byte ANIMATION_BYTE_5 = '5';
+const byte YES_BYTE = 'Y';
+const byte NO_BYTE = 'N';
+
+//----Variable definitions--------------------------------------------
+//****Colour HSV******************************************************
+byte hue = 0;
+byte saturation = 255;
+byte value = 100;
+byte animation_byte = 'x';
+
+//Time and counters
+int delay_anim = 2;
+int theme_hue_counter = 0;
+int rainbow_counter = 0;
+float animation_start_time = -1;
+float default_animation_start_time = -1;
+float relay_delay_time = -1;
+float relay_start_time = -1;
+
+//Flags
+bool relay_start_flag = false;
+
+//Absolut theme hues
+const int THEME_HUE_BLUE = 160;
+const int THEME_HUE_PURPLE = 190;
+const int THEME_HUE_PINK = 224;
+const int THEME_HUE_AQUA = 128;
+const int THEME_HUE_YELLOW = 35;
+
+int theme_hues[] = {THEME_HUE_BLUE, THEME_HUE_PURPLE, THEME_HUE_PINK, THEME_HUE_AQUA, THEME_HUE_YELLOW};
+
+int rgb_pin_matrix[6][3] = {
+  {R_1_PWM_PIN, G_1_PWM_PIN, B_1_PWM_PIN},
+  {R_2_PWM_PIN, G_2_PWM_PIN, B_2_PWM_PIN},
+  {R_3_PWM_PIN, G_3_PWM_PIN, B_3_PWM_PIN},
+  {R_4_PWM_PIN, G_4_PWM_PIN, B_4_PWM_PIN},
+  {R_5_PWM_PIN, G_5_PWM_PIN, B_5_PWM_PIN},
+  {R_6_PWM_PIN, G_6_PWM_PIN, B_6_PWM_PIN}
+};
+
+//**********Functions******************************************************************************
+void debug_sequence();
+void event_check();
+void default_1();
+void default_2();
+void default_3();
+void default_4();
+void default_5();
+void default_6();
+
+void animation_1();
+void animation_2();
+void animation_3();
+void animation_4();
+void animation_5();
+void clear_frames();
+
+void setup() {
+  Serial.begin(9600);
+  for (int i = 0; i < 6; i++) {
+    for (int j = 0; j < 3; j++) {
+      pinMode(rgb_pin_matrix[i][j], OUTPUT);
+      delay(10);
+    }
+  }
+  pinMode(DEBUG_BUTTON_PIN, INPUT);
+  pinMode(RELAY_PIN, OUTPUT);
+  delay(1000);
+  debug_sequence();   //TODO
+}
+
+void loop() {
+  clear_frames();
+  default_1();
+  clear_frames();
+  default_2();
+  clear_frames();
+  default_3();
+  clear_frames();
+  default_4();
+  clear_frames();
+  default_5();
+  clear_frames();
+  default_6();
+}
+
+//-------Event check-----------------------------------------------------------------------------------
+void event_check() {
+  check_serial_incoming();
+  if (digitalRead(DEBUG_BUTTON_PIN) == HIGH) {
+    if (DEBUG_PRINTLN) {
+      Serial.println("event check");
+    }
+    debug_sequence();
+  }
+}
+
+//-------Serial comms---------------------------------------------------------------------------------
+void check_serial_incoming() {
+  if (Serial.available()) {
+    animation_byte = Serial.read();
+    if (animation_byte == ANIMATION_BYTE_1) {
+      Serial.write('Y');
+      animation_1();
+    }
+    else if (animation_byte == ANIMATION_BYTE_2) {
+      Serial.write('Y');
+      animation_2();
+    }
+    else if (animation_byte == ANIMATION_BYTE_3) {
+      Serial.write('Y');
+      animation_3();
+    }
+    else if (animation_byte == ANIMATION_BYTE_4) {
+      Serial.write('Y');
+      animation_4();
+    }
+    else if (animation_byte == ANIMATION_BYTE_5) {
+      Serial.write('Y');
+      animation_5();
+    }
+    else {
+      Serial.write('N');
+    }
+  }
+  while (Serial.available()) {  //clear buffer
+    Serial.read();
+  }
+}
+
+//-----------------Debugging------------------------------------------------------------------
+void debug_sequence() {
+  for (int i = 0; i < 6; i++) {
+    for (int j = 0; j < 3; j++) {
+      analogWrite(rgb_pin_matrix[i][j], 0);
+      delay(1);
+    }
+  }
+  for (int i = 0; i < 6; i++) {
+    for (int j = 0; j < 3; j++) {
+      analogWrite(rgb_pin_matrix[i][j], 255);
+      delay(1000);
+      analogWrite(rgb_pin_matrix[i][j], 0);
+    }
+  }
+}
+
+
+//-----------------Show lights----------------------------------------------------------------
+void showAnalogRGB( const CRGB& rgb, int frame) {
+  analogWrite(rgb_pin_matrix[frame][0], rgb.r );
+  analogWrite(rgb_pin_matrix[frame][1], rgb.g );
+  analogWrite(rgb_pin_matrix[frame][2], rgb.b );
+}
+
+//--------------------------Animations-----------------------------------------------------------------------------------------------
+//---------------------Default animations-------------------------------------------------------------------------------------------
+void clear_frames() {
+  for (int i = 0; i < 6; i++) {
+    for (int j = 0; j < 3; j++) {
+      analogWrite(rgb_pin_matrix[i][j], 0);
+    }
+  }
+}
+
+//--------------default 1-----------------------------------------------------------------------------------------------------------
+void default_1() {      //scrolling rainbow
+  //  int rainbow_colours[] = {0, 15, 50, 96, 160, 192, 224};
+  int rainbow_colours[] = {10, 50, 94, 135, 180, 220};
+  int colour_counter = 0;
+  animation_start_time = millis();
+  while (millis() - animation_start_time <= ANIMATION_TIME) {
+    for (int f = 0; f < 6; f++) {
+      showAnalogRGB(CHSV( rainbow_colours[colour_counter], 255, 255), f);
+      event_check();
+      delay(delay_anim);
+      colour_counter++;
+      if (colour_counter == 6) {
+        colour_counter = 0;
+      }
+    }
+    colour_counter++;
+    if (colour_counter == 6) {
+      colour_counter = 0;
+    }
+    event_check();
+    delay(delay_anim * 50);
+  }
+}
+
+//--------------default 2-----------------------------------------------------------------------------------------------------------
+void default_2() {    //Green_blue swopping
+  int animation_1_colours_midpoint = 123;
+  default_animation_start_time = millis();
+  while (millis() - default_animation_start_time <= DEFAULT_ANIMATION_TIME) {
+    for (int i = 90; i <= 156; i++) {
+      for (int f = 0; f < 6; f++) {
+        if (f == 0 || f == 2 || f == 4) {
+          hue = i;
+          showAnalogRGB(CHSV(hue, 255, 255), f);
+          event_check();
+        }
+        else {
+          hue = animation_1_colours_midpoint - (i - animation_1_colours_midpoint);
+          showAnalogRGB(CHSV(hue, 255, 255), f);
+          event_check();
+        }
+      }
+      delay(delay_anim);
+    }
+    delay(delay_anim);
+    for (int r = 156; r >= 90; r--) {
+      for (int f = 0; f < 6; f++) {
+        if (f == 0 || f == 2 || f == 4) {
+          hue = r;
+          showAnalogRGB(CHSV(hue, 255, 255), f);
+          event_check();
+        }
+        else {
+          hue = animation_1_colours_midpoint + (animation_1_colours_midpoint - r);
+          showAnalogRGB(CHSV(hue, 255, 255), f);
+          event_check();
+        }
+        delay(delay_anim);
+      }
+      delay(delay_anim);
+    }
+  }
+}
+
+//--------------default 3-----------------------------------------------------------------------------------------------------------
+void default_3() {      //scrolling yellow & purple stripes
+  int rainbow_colours[] = {THEME_HUE_PURPLE, THEME_HUE_PURPLE, THEME_HUE_PURPLE, THEME_HUE_YELLOW, THEME_HUE_YELLOW, THEME_HUE_YELLOW};
+  int colour_counter = 0;
+  animation_start_time = millis();
+  while (millis() - animation_start_time <= ANIMATION_TIME) {
+    for (int f = 0; f < 6; f++) {
+      showAnalogRGB(CHSV( rainbow_colours[colour_counter], 255, 255), f);
+      event_check();
+      delay(delay_anim);
+      colour_counter++;
+      if (colour_counter == 6) {
+        colour_counter = 0;
+      }
+    }
+    colour_counter++;
+    if (colour_counter == 6) {
+      colour_counter = 0;
+    }
+    event_check();
+    delay(delay_anim * 50);
+  }
+}
+
+//--------------default 4-----------------------------------------------------------------------------------------------------------
+void default_4() {   //strobe lights = expressive
+  //  hue = theme_hues[1];
+  int random_hue = random(0, 4);
+  int random_frame_1 = random(0, 6);
+  int random_frame_2 = random(0, 6);
+  int frame_pulse_timer = 0;
+  default_animation_start_time = millis();
+  while (millis() - default_animation_start_time <= DEFAULT_ANIMATION_TIME) {
+    random_hue = random(0, 4);
+    random_frame_1 = random(0, 6);
+    random_frame_2 = random(0, 6);
+    event_check();
+    frame_pulse_timer = millis();
+    while (millis() - frame_pulse_timer < 200) {
+      showAnalogRGB(CHSV(theme_hues[random_hue], 255, 255), random_frame_1);
+      showAnalogRGB(CHSV(theme_hues[random_hue], 255, 255), random_frame_2);
+      event_check();
+    }
+    showAnalogRGB(CHSV(0, 0, 0), random_frame_1);
+    showAnalogRGB(CHSV(0, 0, 0), random_frame_2);
+    event_check();
+  }
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------
+void default_5() {
+  default_animation_start_time = millis();
+  while (millis() - default_animation_start_time <= DEFAULT_ANIMATION_TIME) {
+    for (int h = 0; h < 5; h++) {
+      hue = theme_hues[h];
+      for (int i = 30; i <= 255; i++) {
+        for (int f = 0; f < 6; f++) {
+          showAnalogRGB(CHSV( hue, 255, i), f);
+          event_check();
+        }
+        delay(delay_anim * 2);
+      }
+      delay(delay_anim * 100);
+      for (int r = 255; r >= 30; r--) {
+        for (int f = 0; f < 6; f++) {
+          showAnalogRGB(CHSV( hue, 255, r), f);
+          event_check();
+        }
+        delay(delay_anim * 2);
+      }
+      delay(delay_anim * 10);
+    }
+  }
+}
+
+//--------------default 6-----------------------------------------------------------------------------------------------------------
+void default_6() {    //purple,blue scrolling
+  int animation_5_values[] = {255, 205, 155, 105, 55, 0};
+  int value_counter = 0;
+  animation_start_time = millis();
+  while (millis() - animation_start_time <= ANIMATION_TIME) {
+    for(int h= 0; h<2; h++){
+    for (int f = 0; f < 6; f++) {
+      showAnalogRGB(CHSV(theme_hues[h], 255, animation_5_values[value_counter]), f);
+      event_check();
+      delay(delay_anim * 2);
+      value_counter++;
+      if (value_counter == 6) {
+        value_counter = 0;
+      }
+    }
+    value_counter++;
+    if (value_counter == 6) {
+      value_counter = 0;
+    }
+    delay(delay_anim * 100);
+    }
+  }
+}
+
+//---------------------Triggered animations-------------------------------------------------------
+//--------------------------Animation 1-----------------------------------------------------------
+void animation_1() {    //pulsing blues and greens - human
+  int animation_1_colours[] = {96, 108, 120, 130, 140, 150, 160};
+  int random_colour = random(0, 6);
+  int random_frame = random(0, 6);
+  int frame_pulse_timer = 0;
+  animation_start_time = millis();
+  while (millis() - animation_start_time <= ANIMATION_TIME) {
+    if (millis() - animation_start_time < RELAY_DELAY_TIME) {
+      relay_start_flag = false;
+    }
+    else {
+      relay_start_time = millis();
+      relay_start_flag = true;
+    }
+    if ((relay_start_flag == true) && (millis() - relay_start_time <= RELAY_START_TIME)) {
+      digitalWrite(RELAY_PIN, HIGH);
+    }
+    else {
+      digitalWrite(RELAY_PIN, LOW);
+      relay_start_flag = false;
+    }
+    random_colour = random(0, 6);
+    random_frame = random(0, 6);
+    frame_pulse_timer = millis();
+    while (millis() - frame_pulse_timer < 200) {
+      showAnalogRGB(CHSV(animation_1_colours[random_colour], 255, 255), random_frame);
+    }
+  }
+}
+
+//--------------------------Animation 2-----------------------------------------------------------
+void animation_2() {   //strobe lights = expressive
+  hue = theme_hues[1];
+  int random_frame_1 = random(0, 6);
+  int random_frame_2 = random(0, 6);
+  int frame_pulse_timer = 0;
+  for (int i = 0; i < 6; i++) {
+    for (int j = 0; j < 3; j++) {
+      analogWrite(rgb_pin_matrix[i][j], 0);
+    }
+  }
+  animation_start_time = millis();
+  while (millis() - animation_start_time <= ANIMATION_TIME) {
+    random_frame_1 = random(0, 6);
+    random_frame_2 = random(0, 6);
+    frame_pulse_timer = millis();
+    while (millis() - frame_pulse_timer < 200) {
+      showAnalogRGB(CHSV(hue, 255, 255), random_frame_1);
+      showAnalogRGB(CHSV(hue, 255, 255), random_frame_2);
+    }
+    showAnalogRGB(CHSV(hue, 255, 0), random_frame_1);
+    showAnalogRGB(CHSV(hue, 255, 0), random_frame_2);
+  }
+}
+
+//--------------------------Animation 3-----------------------------------------------------------
+void animation_3() {    //Scrolling rainbow = equal
+  int animation_3_colours[] = {0, 15, 50, 96, 160, 192};
+  int colour_counter = 0;
+  animation_start_time = millis();
+  while (millis() - animation_start_time <= ANIMATION_TIME) {
+    for (int f = 0; f < 6; f++) {
+      showAnalogRGB(CHSV( animation_3_colours[colour_counter], 255, 255), f);
+      delay(delay_anim);
+      colour_counter++;
+      if (colour_counter == 6) {
+        colour_counter = 0;
+      }
+    }
+    colour_counter++;
+    if (colour_counter == 6) {
+      colour_counter = 0;
+    }
+    delay(delay_anim * 50);
+  }
+}
+
+//--------------------------Animation 4-----------------------------------------------------------
+void animation_4() {  //pulsing aqua - love
+  hue = theme_hues[3];
+  animation_start_time = millis();
+  while (millis() - animation_start_time <= ANIMATION_TIME) {
+    for (int i = 30; i <= 255; i++) {
+      for (int f = 0; f < 6; f++) {
+        showAnalogRGB(CHSV( hue, 255, i), f);
+      }
+      delay(delay_anim * 2);
+    }
+    delay(delay_anim * 100);
+    for (int r = 255; r >= 30; r--) {
+      for (int f = 0; f < 6; f++) {
+        showAnalogRGB(CHSV( hue, 255, r), f);
+      }
+      delay(delay_anim * 2);
+    }
+    delay(delay_anim * 10);
+  }
+}
+
+//--------------------------Animation 5-----------------------------------------------------------
+void animation_5() {    //Scrolling yellow = transparency
+  hue = theme_hues[4];
+  int animation_5_values[] = {255, 205, 155, 105, 55, 0};
+  int value_counter = 0;
+  animation_start_time = millis();
+  while (millis() - animation_start_time <= ANIMATION_TIME) {
+    for (int f = 0; f < 6; f++) {
+      showAnalogRGB(CHSV(35, 255, animation_5_values[value_counter]), f);
+      delay(delay_anim * 2);
+      value_counter++;
+      if (value_counter == 6) {
+        value_counter = 0;
+      }
+    }
+    value_counter++;
+    if (value_counter == 6) {
+      value_counter = 0;
+    }
+    delay(delay_anim * 100);
+  }
+}
